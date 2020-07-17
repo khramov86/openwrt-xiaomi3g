df80e312711e1ecd635cdd473df6415cb0637f1f
Vladimir Khramov <khramov.vladimir@gmail.com>
2020-07-17 09:31:16 +0300

    first script version

---
+++ Go-OpenWRT-Breed.cmd
@@ -0,0 +1,12 @@
+
+:: https://4pda.ru/forum/index.php?s=&showtopic=837667&view=findpost&p=76121038
+
+:: Cкачиваем свежую версию OpenWRT.
+SET version=19.07.3
+echo version is %version%
+wget.exe https://downloads.openwrt.org/releases/%version%/targets/ramips/mt7621/openwrt-%version%-ramips-mt7621-xiaomi_mir3g-squashfs-kernel1.bin
+wget.exe https://downloads.openwrt.org/releases/%version%/targets/ramips/mt7621/openwrt-%version%-ramips-mt7621-xiaomi_mir3g-squashfs-rootfs0.bin
+
+Go-OpenWRT-Breed-2.cmd openwrt-%version%-ramips-mt7621-xiaomi_mir3g-squashfs-kernel1.bin openwrt-%version%-ramips-mt7621-xiaomi_mir3g-squashfs-rootfs0.bin
+
+pause
